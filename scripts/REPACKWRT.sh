e93881fc0467ba5f4c93250120f3861596b00c4c
hj-wrt <mincoek76@gmail.com>
2025-08-16 00:49:54 +0700

    Update REPACKWRT.sh

--- scripts/REPACKWRT.sh
+++ scripts/REPACKWRT.sh
@@ -56,7 +56,7 @@ repackwrt() {
     fi
 
     # Define constants
-    local readonly OPHUB_REPO="https://github.com/xidz-repo/amlogic-s9xxx-openwrt/archive/refs/heads/main.zip"
+    local readonly OPHUB_REPO="https://github.com/tes-rep/amlogic-s9xxx-openwrt/archive/refs/heads/main.zip"
     local readonly ULO_REPO="https://github.com/xidz-repo/ULO-Builder/archive/refs/heads/main.zip"
     local readonly work_dir="$GITHUB_WORKSPACE/$WORKING_DIR"
     
